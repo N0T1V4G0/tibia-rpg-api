c43da6e56c609a20d945c8236f125dca0d7ad68b
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-08-01 00:22:43 -0300

    chore: set up prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ItemsModule } from './items/items.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ItemsModule],
   controllers: [AppController],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
